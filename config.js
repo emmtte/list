b389282327d65483cf8db6b4165fa44216c4062b
Emmanuel CHARETTE <emmanuel.charette@gmail.com>
2017-11-12 19:07:52 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,31 @@
+// Google Sheet ID
+// var SPREADSHEET_ID = "YOUR SPREADSHEET ID"
+
+// Binance
+// https://www.binance.com/userCenter/tradeHistory.html
+// var API_Binance_Key = "YOUR BINANCE API KEY"
+// var API_Binance_Secret = "YOUR BINANCE API SECRET"
+ 
+// Bittrex
+// https://bittrex.com/History
+// var API_Bittrex_Key = "YOUR BITTREX API KEY"
+// var API_Bittrex_Secret = "YOUR BITTREX API SECRET"
+
+// Cryptopia
+// https://www.cryptopia.co.nz/TradeHistory
+// var API_Cryptopia_Key = "YOUR CRYPTOPIA API KEY"
+// var API_Cryptopia_Secret = "YOUR CRYPTOPIA API SECRET"
+
+// Kraken
+// https://www.kraken.com/u/history/export
+// var API_Kraken_Key = "YOUR KRAKEN API KEY"
+// var API_Kraken_Secret = "YOUR KRAKEN API SECRET"
+
+// Kucoin
+// https://www.cryptopia.co.nz/TradeHistory
+// var API_Kraken_Key = "YOUR KUCOIN API KEY"
+// var API_Kraken_Secret = "YOUR KUCOIN API SECRET"
+
+// Poloniex
+// var API_Poloniex_Key = "YOUR POLONIEX API KEY"
+// var API_Poloniex_Secret = "YOUR POLONIEX API SECRET"
