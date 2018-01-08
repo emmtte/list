1e072109a448ee771635b6b42aa8b97704875e1e
Emmanuel CHARETTE <emmanuel.charette@gmail.com>
2018-01-08 21:59:20 +0100

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,5 +1,5 @@
 function onOpen() {
   var sheet = SpreadsheetApp.getActiveSpreadsheet();
-  var entries = [{ name : "Update Portfolio", functionName : "balance" }];
+  var entries = [{ name : "Update Portfolio", functionName : "balnew" }];
   sheet.addMenu("Cryptos Tools", entries);
 };
