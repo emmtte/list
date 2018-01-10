ad7323ac2c803a77fadf9ff0062edf2fdde4c4be
Emmanuel CHARETTE <emmanuel.charette@gmail.com>
2018-01-10 23:09:37 +0100

    Delete API_Marketcap.js

--- API_Marketcap.js
+++
@@ -1,8 +0,0 @@
-function coinmarketcap() {
-var url = "https://api.coinmarketcap.com/v1/ticker/?convert=EUR&limit=400";
-//var url = "https://api.coinmarketcap.com/v1/ticker/?convert=EUR";
-var response = UrlFetchApp.fetch(url);
-var text = response.getContentText();
-var obj_array = JSON.parse(text);
-return obj_array;
-}
