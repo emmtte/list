7b74cce2e124f634c62d0b4e5f8066639dd94528
Emmanuel CHARETTE <emmanuel.charette@gmail.com>
2018-02-09 23:24:49 +0100

    Update API_Bitstamp.js

--- API_Bitstamp.js
+++ API_Bitstamp.js
@@ -1,7 +1,7 @@
 //Need beta users
 //Work in progress
 
-// BETA V0.5
+// BETA V0.6
 // I assume that key and secret API are in the "Config" spreadsheet. The key is in cell B30, the secret in cell B31 and customer_id in B32 cell
 
 function Bitstamp() {
@@ -19,14 +19,18 @@ function Bitstamp() {
  shaObj.update(message);
  var signature = shaObj.getHMAC("HEX").toString().toUpperCase();
 
-var url = "http://www.bitstamp.net/api/balance/"
+var url = "http://www.bitstamp.net/api/v2/balance/"
 cb = "key=" + key + "&signature=" + signature + "&nonce=" + nonce;
 
-//var options = { method: 'post' };
+var formData = {
+   'key': key,
+   'signature': signature,
+   'nonce': nonce
+ };
 
 var options = {
   "method" : "post",
-  "payload": cb,
+  "payload": formData,
   "muteHttpExceptions" : true
  };
 
@@ -36,5 +40,6 @@ var data = JSON.parse(response.getContentText());
 Logger.log(data);
   
 //please give me result (data from log)
+
 
 }
